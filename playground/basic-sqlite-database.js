135d9b5e48b070c7ca1a8404c2d2f8b736da6d32
Paulo Oliveira <paulinho@Paulos-iMac.local>
2019-03-27 15:41:54 -0400

    Install sequelize and sqlite3

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -1,4 +1,4 @@
-var Sequelize = require('sequilize');
+var Sequelize = require('sequelize');
 var sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect' : 'sqlite',
     'storage' : 'basic-sqlite-database.sqlite'
