8de831ee7b301c1be0a4cf657d2a6ec841d07301
Paulo Oliveira <paulinho@Paulos-iMac.local>
2019-03-27 11:16:57 -0400

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.post('/todos', function (req, res) {
     res.json(todos);
 });
 
+
 //DELETE /todos/:id
 app.delete('/todos/:id', function (req, res){
     var todoId = parseInt(req.params.id, 10);
@@ -69,6 +70,79 @@ app.delete('/todos/:id', function (req, res){
 });
 
 
+//PUT /todos/:id
+app.put('/todos/:id', function (req,res) {
+    
+    var todoId = parseInt(req.params.id, 10);
+    var todoItem = _.findWhere(todos, {id: todoId});
+    
+    if (!todoItem){
+        return res.status(404).json({"error":"Id not found"})
+    }
+    
+    var body = _.pick(req.body, 'description', 'completed');
+    var valideProperties = {};
+    
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        valideProperties.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+    
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        valideProperties.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+    
+    _.extend(todoItem, valideProperties);
+    
+    res.json(todoItem);
+    
+})
+
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT)
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
