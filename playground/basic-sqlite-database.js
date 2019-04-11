5d8da2b18cd3f373c20b1c299d3c9197b80f696a
Paulo Oliveira <paulinho@Paulos-iMac.local>
2019-04-11 15:23:06 -0400

    Add todo model and POST /todos

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -1,9 +1,34 @@
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize(undefined, undefined, undefined, {
     'dialect' : 'sqlite',
-    'storage' : 'basic-sqlite-database.sqlite'
+    'storage' : __dirname + '/basic-sqlite-database.sqlite'
 });
+
+var Todo = sequelize.define('todo', {
+    description : {
+        type : Sequelize.STRING, 
+        allowNull : false,
+        validate : {
+            len : [1, 250]
+        }
+    }, 
+    completed : {
+        type : Sequelize.BOOLEAN,
+        allowNull : false,
+        defaultValue : false
+    }
+})
 
 sequelize.sync().then(function () {
     console.log('Everything is synced');
-})
+    
+    Todo.create({
+        description : 'Comprar o pao',
+    }).then(function(todo) {
+        console.log(todo.toJSON());
+        console.log('Finished');
+    })
+    .catch(function (e) {
+        console.log(e);
+    });
+});
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -60,17 +61,23 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
     
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
+    db.todo.create(body).then(function(todo) {
+        res.json(todo);
+    }, function(){
+        res.status(400).send();
+    })
     
-    body.description = body.description.trim();
-    
-    body.id = todoNext++;
-    
-    todos.push(body);
-    
-    res.json(todos);
+//    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+//        return res.status(400).send();
+//    }
+//    
+//    body.description = body.description.trim();
+//    
+//    body.id = todoNext++;
+//    
+//    todos.push(body);
+//    
+//    res.json(todos);
 });
 
 
@@ -119,9 +126,13 @@ app.put('/todos/:id', function (req,res) {
     
 })
 
-app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT)
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function () {
+        console.log('Express listening on port ' + PORT)
+    });
 });
+
+
 
 
 
